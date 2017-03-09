ce51986c89cdfc0e2a0a4b7a30bd7aa2b9f7603d
Aditya <aditya999123@gmail.com>
2017-03-09 06:29:08 +0530

    updates

--- project/views.py
+++ project/views.py
@@ -16,21 +16,23 @@ def project(request):
 	for o in project_data.objects.all().order_by('title'):
 		if(o.active==True):
 			if flag==0:
-				project_temp_string="""<B><u>a.)%s:</u></b>%s<br>"""
+				project_temp_string="""<B style="font-size:1.6em;position:relative;left:.1em;">	&#9755%s:</b><hr><div style="font-size:1.5em;">%s</div>"""
 				flag=1	
 				temp_obj=o
 				project_string+=project_temp_string % (o.title,o.content)
 			else:
 				if  o.title==temp_obj.title:
-					project_temp_string="""<B></b>%s<br>"""
+					project_temp_string="""<B style="font-size:1.9em;float:left;"></b><div style="font-size:1.5em;">%s</div><hr>"""
 					project_string+=project_temp_string % (o.content)
 				else:
-					project_temp_string="""<B><u>a.)%s:</u></b>%s<br>"""
+					project_temp_string="""<B style="font-size:1.9em;">	&#9755%s:<hr></b><div style="font-size:1.5em;">%s</div>"""
 					project_string+=project_temp_string % (o.title,o.content)
 					temp_obj=o
+			print "project added"
+	print project_string
 	if request.user.is_authenticated():
 		return render(request,'academics.html' ,{'project_string':project_string,'link1':'<a href="/profile/">PROFILE</a>','link2':'<a href="/logout/">LOGOUT</a>'})
 	else:
-		return render(request,'academics.html',{'link2':'<a href="/login/">LOGIN</a>'})
+		return render(request,'academics.html',{'project_string':project_string,'link2':'<a href="/login/">LOGIN</a>'})
 	# except:
 	# 	return render(request,'activities.html' ,{'announcement_string':'No upcoming activities'})
